cd295d7d820f31ca307b2fff36172d7636692099
Delais Rafael Diaz Lopez <delaisdiaz13@hotmail.com>
2022-09-05 00:55:44 -0500

    Practica de constructores

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -1,13 +1,30 @@
 public class Aritmetica {
 
-    int a,b = 0;
+    //Atributos de la clase
+    int a,b;
 
+
+    // sobrecarga de constructores
+    //Constructor vacio
+    public Aritmetica() {
+        System.out.println("Esto se esta ejecutando desde el constructor");
+    }
+
+    // Contructor con arg
+    public Aritmetica(int a , int b){
+        this.a = a;
+        this.b = b;
+        System.out.println("Esto se esta ejecutando desde el constructor con arg");
+    }
+    //Metodos
     public void sumar() {
         int resultado = a+b;
         System.out.println("El resultado de la suma es: " + resultado);
     }
 
-    public int multiplicar(){
-        return a*b;
+    public int multiplicar(int arg1, int arg2){
+        this.a = arg1;
+        this.b = arg2;
+        return this.a * this.b;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,22 @@ public class Main {
     public static void main(String[] args) {
 
         Aritmetica arithmetic1 = new Aritmetica();
+        System.out.println("arithmetic1 a: " + arithmetic1.a);
+        System.out.println("arithmetic1 b: " + arithmetic1.b);
 
-        arithmetic1.a=1;
+
+        Aritmetica arithmetic2 = new Aritmetica(5,8);
+        System.out.println("arithmetic2 a: " + arithmetic2.a);
+        System.out.println("arithmetic2 b: " + arithmetic2.b);
+
+        /*arithmetic1.a=1;
         arithmetic1.b=2;
         
         arithmetic1.sumar();
-        int result_Multi = arithmetic1.multiplicar();
-        System.out.println("el resultado de la multiplicacion es = " + result_Multi);
+        int result_Multi = arithmetic1.multiplicar(4,5);
+        System.out.println("el resultado de la multiplicacion es = " + result_Multi);*/
+
+
     }
 
 }
